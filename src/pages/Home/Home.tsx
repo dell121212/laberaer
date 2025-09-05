da1d5a05f75b2339d491398ab0388d3611bb329f
dell121212 <104049438+dell121212@users.noreply.github.com>
2025-09-05 11:51:14 +0800

    Fix DutyReminderModal import path

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -23,7 +23,7 @@ import {
 } from 'lucide-react';
 import { format } from 'date-fns';
 import { zhCN } from 'date-fns/locale';
-import DutyReminderModal from '../components/DutyReminderModal';
+import DutyReminderModal from '../../components/DutyReminderModal';
 
 const Home: React.FC = () => {
   const { strains, members, dutySchedules, media, theses, activityLogs } = useApp();
